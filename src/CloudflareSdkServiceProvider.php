10538b182698be91e006df68c72cb7d194aba1c9
Alex Renoki <alex@renoki.org>
2023-09-02 17:15:00 +0300

    Initial configs

---
+++ src/CloudflareSdkServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace RenokiCo\CloudflareSdk;
+
+use Illuminate\Support\ServiceProvider;
+
+class CloudflareSdkServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace RenokiCo\:package_namespace\Test;
+namespace RenokiCo\CloudflareSdk\Test;
 
 use Orchestra\Testbench\TestCase as Orchestra;
 
@@ -13,13 +13,7 @@ abstract class TestCase extends Orchestra
     {
         parent::setUp();
 
-        $this->resetDatabase();
-
-        $this->loadLaravelMigrations(['--database' => 'sqlite']);
-
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-
-        $this->withFactories(__DIR__.'/database/factories');
+        //
     }
 
     /**
@@ -38,22 +32,5 @@ abstract class TestCase extends Orchestra
     public function getEnvironmentSetUp($app)
     {
         $app['config']->set('app.key', 'wslxrEFGWY6GfGhvN9L3wH3KSRJQQpBD');
-        $app['config']->set('auth.providers.users.model', Models\User::class);
-        $app['config']->set('database.default', 'sqlite');
-        $app['config']->set('database.connections.sqlite', [
-            'driver'   => 'sqlite',
-            'database' => __DIR__.'/database.sqlite',
-            'prefix'   => '',
-        ]);
-    }
-
-    /**
-     * Reset the database.
-     *
-     * @return void
-     */
-    protected function resetDatabase()
-    {
-        file_put_contents(__DIR__.'/database.sqlite', null);
     }
 }
